600adaaef0d547fa6895273040afecf45714efbc
Kagol <kagol@sina.com>
2024-04-29 23:17:41 +0800

    增加部分文章

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -6,6 +6,18 @@ export function sidebarJunior() {
       items: [
         { text: '春', link: '/junior/spring' }
       ]
+    },
+    {
+      text: '七年级下册',
+      collapsed: true,
+      items: [
+        { text: '河中石兽', link: '/junior/hezhongshishou' },
+        { text: '陋室铭', link: '/junior/loushimin' },
+        { text: '木兰诗', link: '/junior/mulanshi' },
+        { text: '驿路梨花', link: '/junior/yilulihua' },
+        { text: '紫藤萝瀑布', link: '/junior/zitengluopubu' },
+        { text: '最苦与最乐', link: '/junior/zuikuyuzuile' },
+      ]
     }
   ]
 }
