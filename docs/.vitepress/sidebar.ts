0122c944460d18d47224a0232af5a64b48ca81b6
Kagol <kagol@sina.com>
2024-05-09 22:49:42 +0800

    标记背诵

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -18,18 +18,74 @@ export function sidebarJunior() {
         { text: '紫藤萝瀑布', link: '/junior/zitengluopubu' },
         { text: '最苦与最乐', link: '/junior/zuikuyuzuile' },
       ]
-    }
+    },
+    {
+      text: '八年级上册',
+      collapsed: true,
+      items: [
+        { text: '三峡', link: '/junior/sanxia' }
+      ]
+    },
+    {
+      text: '八年级下册',
+      collapsed: true,
+      items: [
+        { text: '桃花源记', link: '/junior/taohuayuanji' }
+      ]
+    },
+    {
+      text: '九年级上册',
+      collapsed: true,
+      items: [
+        { text: '岳阳楼记', link: '/junior/yueyanglouji' }
+      ]
+    },
+    {
+      text: '九年级下册',
+      collapsed: true,
+      items: [
+        { text: '祖国啊，我亲爱的祖国', link: '/junior/zuguo' }
+      ]
+    },
   ]
 }
 
 export function sidebarSenior() {
   return [
     {
-      text: '高一',
+      text: '必修1',
       collapsed: true,
       items: [
-        { text: 'xxx', link: '/senior/xxx' }
+        { text: '沁园春·长沙', link: '/senior/changsha' }
       ]
-    }
+    },
+    {
+      text: '必修2',
+      collapsed: true,
+      items: [
+        { text: '兰亭集序', link: '/senior/lantingjixu' }
+      ]
+    },
+    {
+      text: '必修3',
+      collapsed: true,
+      items: [
+        { text: '蜀道难', link: '/senior/shudaonan' }
+      ]
+    },
+    {
+      text: '必修4',
+      collapsed: true,
+      items: [
+        { text: '念奴娇·赤壁怀古', link: '/senior/chibihuaigu' }
+      ]
+    },
+    {
+      text: '必修5',
+      collapsed: true,
+      items: [
+        { text: '归去来兮', link: '/senior/guiqulaixi' }
+      ]
+    },
   ]
 }
