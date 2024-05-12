139950cc1cca276869b5610c3abb8ee569bbca79
Kagol <kagol@sina.com>
2024-05-12 17:11:04 +0800

    增加高中背诵课文

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -4,21 +4,21 @@ export function sidebarJunior() {
       text: '七年级上册',
       collapsed: true,
       items: [
-        { text: '春', link: '/junior/7-1-1-spring' },
-        { text: '古代诗歌四首', link: '/junior/7-1-4' },
-        { text: '从百草园到三味书屋', link: '/junior/7-1-9' },
-        { text: '《论语》十二章', link: '/junior/7-1-11' },
-        { text: '诫子书', link: '/junior/7-1-15' },
-        { text: '天上的街市', link: '/junior/7-1-20' },
+        { text: '⭐ 春', link: '/junior/7-1-1-spring' },
+        { text: '⭐ 古代诗歌四首', link: '/junior/7-1-4' },
+        { text: '⭐ 从百草园到三味书屋', link: '/junior/7-1-9' },
+        { text: '⭐ 《论语》十二章', link: '/junior/7-1-11' },
+        { text: '⭐ 诫子书', link: '/junior/7-1-15' },
+        { text: '⭐ 天上的街市', link: '/junior/7-1-20' },
       ]
     },
     {
       text: '七年级下册',
       collapsed: true,
       items: [
-        { text: '河中石兽', link: '/junior/7-2-24-hezhongshishou' },
-        { text: '短文两篇', link: '/junior/7-2-16' },
-        { text: '木兰诗', link: '/junior/7-2-8-mulanshi' },
+        { text: '⭐ 河中石兽', link: '/junior/7-2-24-hezhongshishou' },
+        { text: '⭐ 短文两篇', link: '/junior/7-2-16' },
+        { text: '⭐ 木兰诗', link: '/junior/7-2-8-mulanshi' },
         { text: '驿路梨花', link: '/junior/7-2-14-yilulihua' },
         { text: '紫藤萝瀑布', link: '/junior/7-2-17-zitengluopubu' },
         { text: '最苦与最乐', link: '/junior/7-2-15-zuikuyuzuile' },
@@ -28,50 +28,50 @@ export function sidebarJunior() {
       text: '八年级上册',
       collapsed: true,
       items: [
-        { text: '三峡', link: '/junior/8-1-9-sanxia' },
-        { text: '短文二篇', link: '/junior/8-1-10' },
-        { text: '与朱元思书', link: '/junior/8-1-11' },
-        { text: '唐诗五首', link: '/junior/8-1-12' },
-        { text: '白杨礼赞', link: '/junior/8-1-14' },
-        { text: '愚公移山', link: '/junior/8-1-22' },
-        { text: '诗词五首', link: '/junior/8-1-24' },
+        { text: '⭐ 三峡', link: '/junior/8-1-9-sanxia' },
+        { text: '⭐ 短文二篇', link: '/junior/8-1-10' },
+        { text: '⭐ 与朱元思书', link: '/junior/8-1-11' },
+        { text: '⭐ 唐诗五首', link: '/junior/8-1-12' },
+        { text: '⭐ 白杨礼赞', link: '/junior/8-1-14' },
+        { text: '⭐ 愚公移山', link: '/junior/8-1-22' },
+        { text: '⭐ 诗词五首', link: '/junior/8-1-24' },
       ]
     },
     {
       text: '八年级下册',
       collapsed: true,
       items: [
-        { text: '桃花源记', link: '/junior/8-2-9-taohuayuanji' },
-        { text: '小石潭记', link: '/junior/8-2-10' },
-        { text: '《诗经》二首', link: '/junior/8-2-12' },
-        { text: '《庄子》二则', link: '/junior/8-2-21' },
-        { text: '《礼记》二则', link: '/junior/8-2-22' },
-        { text: '马说', link: '/junior/8-2-23' },
-        { text: '唐诗二首', link: '/junior/8-2-24' },
+        { text: '⭐ 桃花源记', link: '/junior/8-2-9-taohuayuanji' },
+        { text: '⭐ 小石潭记', link: '/junior/8-2-10' },
+        { text: '⭐ 《诗经》二首', link: '/junior/8-2-12' },
+        { text: '⭐ 《庄子》二则', link: '/junior/8-2-21' },
+        { text: '⭐ 《礼记》二则', link: '/junior/8-2-22' },
+        { text: '⭐ 马说', link: '/junior/8-2-23' },
+        { text: '⭐ 唐诗二首', link: '/junior/8-2-24' },
       ]
     },
     {
       text: '九年级上册',
       collapsed: true,
       items: [
-        { text: '岳阳楼记', link: '/junior/9-1-10-yueyanglouji' },
-        { text: '醉翁亭记', link: '/junior/9-1-11' },
-        { text: '湖心亭看雪', link: '/junior/9-1-12' },
-        { text: '诗词三首', link: '/junior/9-1-13' },
+        { text: '⭐ 岳阳楼记', link: '/junior/9-1-10-yueyanglouji' },
+        { text: '⭐ 醉翁亭记', link: '/junior/9-1-11' },
+        { text: '⭐ 湖心亭看雪', link: '/junior/9-1-12' },
+        { text: '⭐ 诗词三首', link: '/junior/9-1-13' },
       ]
     },
     {
       text: '九年级下册',
       collapsed: true,
       items: [
-        { text: '祖国啊，我亲爱的祖国', link: '/junior/9-2-1-zuguo' },
-        { text: '鱼我所欲也', link: '/junior/9-2-9' },
-        { text: '送东阳马生序', link: '/junior/9-2-11' },
-        { text: '词四首', link: '/junior/9-2-12' },
-        { text: '短文两篇', link: '/junior/9-2-13' },
-        { text: '曹刿论战', link: '/junior/9-2-20' },
-        { text: '出师表', link: '/junior/9-2-22' },
-        { text: '诗词曲五首', link: '/junior/9-2-23' },
+        { text: '⭐ 祖国啊，我亲爱的祖国', link: '/junior/9-2-1-zuguo' },
+        { text: '⭐ 鱼我所欲也', link: '/junior/9-2-9' },
+        { text: '⭐ 送东阳马生序', link: '/junior/9-2-11' },
+        { text: '⭐ 词四首', link: '/junior/9-2-12' },
+        { text: '⭐ 短文两篇', link: '/junior/9-2-13' },
+        { text: '⭐ 曹刿论战', link: '/junior/9-2-20' },
+        { text: '⭐ 出师表', link: '/junior/9-2-22' },
+        { text: '⭐ 诗词曲五首', link: '/junior/9-2-23' },
       ]
     },
   ]
@@ -83,35 +83,53 @@ export function sidebarSenior() {
       text: '必修1',
       collapsed: true,
       items: [
-        { text: '沁园春·长沙', link: '/senior/changsha' }
+        { text: '⭐ 沁园春·长沙', link: '/senior/1-1-changsha' },
+        { text: '⭐ 诗两首', link: '/senior/1-2' },
+        { text: '⭐ 荆轲刺秦王', link: '/senior/1-5' },
+        { text: '⭐ 记念刘和珍君', link: '/senior/1-7' },
       ]
     },
     {
       text: '必修2',
       collapsed: true,
       items: [
-        { text: '兰亭集序', link: '/senior/lantingjixu' }
+        { text: '⭐ 《诗经》两首', link: '/senior/2-4' },
+        { text: '⭐ 诗三首', link: '/senior/2-7' },
+        { text: '⭐ 兰亭集序', link: '/senior/2-8-lantingjixu' },
+        { text: '⭐ 赤壁赋', link: '/senior/2-9' },
+        { text: '⭐ 游褒禅山记', link: '/senior/2-10' },
       ]
     },
     {
       text: '必修3',
       collapsed: true,
       items: [
-        { text: '蜀道难', link: '/senior/shudaonan' }
+        { text: '⭐ 蜀道难', link: '/senior/3-4' },
+        { text: '⭐ 杜甫诗三首', link: '/senior/3-5' },
+        { text: '⭐ 琵琶行·并序', link: '/senior/3-6' },
+        { text: '李商隐诗两首', link: '/senior/3-7' },
+        { text: '⭐ 寡人之于国也', link: '/senior/3-8' },
+        { text: '⭐ 劝学', link: '/senior/3-9' },
+        { text: '⭐ 过秦论', link: '/senior/3-10' },
       ]
     },
     {
       text: '必修4',
       collapsed: true,
       items: [
-        { text: '念奴娇·赤壁怀古', link: '/senior/chibihuaigu' }
+        { text: '柳永词两首', link: '/senior/4-4' },
+        { text: '⭐ 苏轼词两首', link: '/senior/4-5' },
+        { text: '⭐ 辛弃疾词两首', link: '/senior/4-6' },
+        { text: '⭐ 李清照词两首', link: '/senior/4-7' },
       ]
     },
     {
       text: '必修5',
       collapsed: true,
       items: [
-        { text: '归去来兮', link: '/senior/guiqulaixi' }
+        { text: '⭐ 归去来兮辞·并序', link: '/senior/5-4' },
+        { text: '⭐ 滕王阁序', link: '/senior/5-5' },
+        { text: '⭐ 陈情表', link: '/senior/5-7' },
       ]
     },
   ]
